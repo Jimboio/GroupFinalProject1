0da6dde6ea64b9fa36ae0b7c15fd1d06b0185e30
Nicklas Gozowsky <nickgozowsky1234@gmail.com>
2024-12-07 04:19:26 -0500

    Initial commit for NicklasWAProject

--- Models/TeamMember.cs
+++ Models/TeamMember.cs
@@ -2,14 +2,10 @@ namespace NicklasWAProject.Models
 {
     public class TeamMember
     {
-        public int Id { get; set; } // Primary key
-        
-        public string FullName { get; set; } // The member's full name
-        
-        public DateTime Birthdate { get; set; } // Birthdate of the member
-        
-        public string CollegeProgram { get; set; } // The college program the member is enrolled in
-        
-        public string YearInProgram { get; set; } // The year or level in the program (e.g., Freshman, Sophomore, etc.)
+        public int Id { get; set; }      
+        public string FullName { get; set; } 
+        public DateTime Birthdate { get; set; } 
+        public string CollegeProgram { get; set; }      
+        public string YearInProgram { get; set; } 
     }
 }
